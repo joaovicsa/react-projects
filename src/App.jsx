45c04ccbde0e34176b3176e162574d783c22786b
João Sá <joao.vsaa90@gmail.com>
2024-05-02 16:29:29 -0300

    add: Adding the Post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+/** @format */
+
+import Post from './components/Post'
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return <Post />
 }
 
-export default App;
+export default App
---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+/** @format */
+
+function Post() {
+  return (
+    <div>
+      <p>João</p>
+      <p>React.js is awesome!</p>
+    </div>
+  )
+}
+
+export default Post
