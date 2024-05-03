77450f692735ec10e56c52ee8f3396e35641d5d4
João Sá <joao.vsaa90@gmail.com>
2024-05-03 20:58:18 -0300

    feat: Trying Dynamic data in Posts

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@
 import Post from './components/Post'
 
 function App() {
-  return <Post />
+  return (
+    <main>
+      <Post author="João" body="This is React.js!" />
+      <Post author="Sebas" body="How can I help you, Sir?" />
+    </main>
+  )
 }
 
 export default App
