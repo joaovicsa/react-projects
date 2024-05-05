c7678df4892b537bbe74f9d337479d11ad5d794d
João Sá <joao.vsaa90@gmail.com>
2024-05-05 16:55:02 -0300

    feat: add New Post component and styles

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,14 +1,18 @@
 /** @format */
 
+import NewPost from './NewPost'
 import Post from './Post'
 import classes from './PostsList.module.css'
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="João" body="This is React.js!" />
-      <Post author="Sebas" body="How can I help you, Sir?" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="João" body="This is React.js!" />
+        <Post author="Sebas" body="How can I help you, Sir?" />
+      </ul>
+    </>
   )
 }
 
