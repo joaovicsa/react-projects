41a33b975958c6cd9027056d6b38038c793c12d3
João Sá <joao.vsaa90@gmail.com>
2024-05-10 18:53:31 -0300

    feat: add empty post message

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -20,11 +20,19 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        {posts.map((post) => (
-          <Post key={post.body} author={post.author} body={post.body} />
-        ))}
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h2>There are no post yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   )
 }
