adcd2b6fbd618f229af11a2986d9560259686cc7
João Sá <joao.vsaa90@gmail.com>
2024-05-10 18:51:14 -0300

    feat: adding post dynamically by using the New Post form

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,22 +1,29 @@
 /** @format */
 
-import { useState } from 'react'
-
 import Post from './Post'
 import NewPost from './NewPost'
 import Modal from './Modal'
 import classes from './PostsList.module.css'
+import { useState } from 'react'
 
 function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([])
+
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts])
+  }
+
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author="Manuel" body="Check out the full course!" />
+        {posts.map((post) => (
+          <Post key={post.body} author={post.author} body={post.body} />
+        ))}
       </ul>
     </>
   )
