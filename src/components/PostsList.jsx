1195a1baea9c35c8a0c5e26fa1d4cd6faf65bbbc
João Sá <joao.vsaa90@gmail.com>
2024-05-10 10:51:52 -0300

    refactor: lifting state up in NewPost

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,26 @@
 /** @format */
 
+import Modal from './Modal'
 import NewPost from './NewPost'
 import Post from './Post'
 import classes from './PostsList.module.css'
 
 function PostsList() {
+   const [enteredBody, setEnteredBody] = useState('')
+   const [enteredAuthor, setEnteredAuthor] = useState('')
+
+   function bodyChangeHandler(event) {
+     setEnteredBody(event.target.value)
+   }
+
+   function authorChangeHandler(event) {
+     setEnteredAuthor(event.target.value)
+   }
   return (
     <>
-      <NewPost />
+      <Modal>
+        <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+      </Modal>
       <ul className={classes.posts}>
         <Post author="João" body="This is React.js!" />
         <Post author="Sebas" body="How can I help you, Sir?" />
