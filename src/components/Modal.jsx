c5adcd0882a10356d1994fdc157c098a6e77ef29
João Sá <joao.vsaa90@gmail.com>
2024-05-10 11:49:14 -0300

    refactor: updating Modal to form-submission lesson

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,11 +5,10 @@ import classes from './Modal.module.css'
 function Modal({ children, onClose }) {
   return (
     <>
-      <div className={classes.backdrop} onClick={onClose}>
-        <dialog open className={classes.modal}>
-          {children}
-        </dialog>
-      </div>
+      <div className={classes.backdrop} onClick={onClose} />
+      <dialog open className={classes.modal}>
+        {children}
+      </dialog>
     </>
   )
 }
