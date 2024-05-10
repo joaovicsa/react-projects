71c9a292aefaff1d5a7bbbd92072a6b91f4c4201
João Sá <joao.vsaa90@gmail.com>
2024-05-10 11:43:37 -0300

    refactor: updating PostList to form-submission lesson

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,37 +1,22 @@
 /** @format */
 
 import { useState } from 'react'
-import Modal from './Modal'
-import NewPost from './NewPost'
+
 import Post from './Post'
+import NewPost from './NewPost'
+import Modal from './Modal'
 import classes from './PostsList.module.css'
 
 function PostsList({ isPosting, onStopPosting }) {
-  const [enteredBody, setEnteredBody] = useState('')
-  const [enteredAuthor, setEnteredAuthor] = useState('')
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value)
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value)
-  }
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
-
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
-        <Post author="Sebas" body="How can I help you, Sir?" />
+        <Post author="Manuel" body="Check out the full course!" />
       </ul>
     </>
   )
