c2d999b7bf46cae971a700955d419915389da860
João Sá <joao.vsaa90@gmail.com>
2024-05-10 11:10:55 -0300

    feat: adding conditional hiding effect to New Post

---
+++ src/components/Modal.jsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import classes from './Modal.module.css'
+
+function Modal({ children, onClose }) {
+  return (
+    <>
+      <div className={classes.backdrop} onClick={onClose}>
+        <dialog open className={classes.modal}>
+          {children}
+        </dialog>
+      </div>
+    </>
+  )
+}
+
+export default Modal
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,28 +1,49 @@
 /** @format */
 
+import { useState } from 'react'
 import Modal from './Modal'
 import NewPost from './NewPost'
 import Post from './Post'
 import classes from './PostsList.module.css'
 
 function PostsList() {
-   const [enteredBody, setEnteredBody] = useState('')
-   const [enteredAuthor, setEnteredAuthor] = useState('')
+  const [modalIsVisible, setModalIsVisible] = useState(true)
+  const [enteredBody, setEnteredBody] = useState('')
+  const [enteredAuthor, setEnteredAuthor] = useState('')
 
-   function bodyChangeHandler(event) {
-     setEnteredBody(event.target.value)
-   }
+  function hideModalHandler() {
+    setModalIsVisible(false)
+  }
 
-   function authorChangeHandler(event) {
-     setEnteredAuthor(event.target.value)
-   }
-  return (
-    <>
-      <Modal>
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value)
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value)
+  }
+
+  let modalContent
+  if (modalIsVisible) {
+    modalContent = (
+      <Modal onClose={hideModalHandler}>
         <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
       </Modal>
+    )
+  }
+  return (
+    <>
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
+
       <ul className={classes.posts}>
-        <Post author="João" body="This is React.js!" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Sebas" body="How can I help you, Sir?" />
       </ul>
     </>
