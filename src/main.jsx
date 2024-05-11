58eeafdfa153aa6e385966be6078241de7880cec
João Sá <joao.vsaa90@gmail.com>
2024-05-11 16:42:36 -0300

    feat: adding dynamic routes to Posts

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './index.css'
 import NewPost, { action as newPostAction } from './routes/NewPost'
 import RootLayout from './routes/RootLayout'
 import Posts, { loader as postsLoader } from './routes/Posts'
+import PostDetails, { loader as postDetailsLoader } from './routes/PostDetails'
 
 const router = createBrowserRouter([
   {
@@ -17,7 +18,10 @@ const router = createBrowserRouter([
         path: '/',
         element: <Posts />,
         loader: postsLoader,
-        children: [{ path: '/create-post', element: <NewPost />, action: newPostAction }],
+        children: [
+          { path: '/create-post', element: <NewPost />, action: newPostAction },
+          { path: '/:id', element: <PostDetails />, loader: postDetailsLoader },
+        ],
       },
     ],
   },
