e2467e2a87bdb6d7c768ee3f0192780cedcf03b6
João Sá <joao.vsaa90@gmail.com>
2024-05-11 15:31:03 -0300

    refactor: refactoring route components & nesting

--- src/routes/Posts.jsx
+++ src/routes/Posts.jsx
@@ -1,8 +1,8 @@
 /** @format */
 
 import { useState } from 'react'
-import MainHeader from './components/MainHeader'
-import PostsList from './components/PostsList'
+import MainHeader from '../components/MainHeader'
+import PostsList from '../components/PostsList'
 
 function App() {
   const [modalIsVisible, setModalIsVisible] = useState(false)
