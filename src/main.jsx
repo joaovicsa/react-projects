79da5f895f27b5120023d95c83e8c29662934430
João Sá <joao.vsaa90@gmail.com>
2024-05-11 15:39:42 -0300

    refactor: nesting New Posts into Posts

--- src/main.jsx
+++ src/main.jsx
@@ -13,8 +13,11 @@ const router = createBrowserRouter([
     path: '/',
     element: <RootLayout />,
     children: [
-      { path: '/', element: <Posts /> }, // our domain
-      { path: '/create-post', element: <NewPost /> },
+      {
+        path: '/',
+        element: <Posts />,
+        children: [{ path: '/create-post', element: <NewPost /> }],
+      },
     ],
   },
 ])
